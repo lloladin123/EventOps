2bf5741815042d36d007e73fc593f607a83909d7
lloladin123 <blackiealexander@yahoo.dk>
2026-02-11 15:22:20 +0100

    Fixed: import uppercase of admin.

--- app/api/upload-slots/route.ts
+++ app/api/upload-slots/route.ts
@@ -1,7 +1,7 @@
 // app/api/upload-slots/route.ts
 export const runtime = "nodejs";
 
-import { getAdmin } from "@/lib//firebase/Admin";
+import { getAdmin } from "@/lib//firebase/admin";
 import { NextResponse } from "next/server";
 
 type Body = {
